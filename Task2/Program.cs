e2b8097c92b82dcfa3cc1b48109a699a9e4e3219
burmasovanton <looseangel1@gmail.com>
2022-09-01 01:01:45 +0500

    Initial commit

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число:");
+int a = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+int b = Console.ReadLine();
+
+if(a > b)
+  {
+  Console.WriteLine("Первое число большее, а второе меньшее.");
+  }
+if(a < b)
+  {
+   Console.WriteLine("Второе число большее, а первое меньшее.");
+  }
+if(a == b)
+  {
+    Console.WriteLine("Первое число и второе число равны.");  
+  }
