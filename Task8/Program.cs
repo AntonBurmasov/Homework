75c51d881e8889aa90ee113494e2fbd0608c5673
burmasovanton <looseangel1@gmail.com>
2022-09-01 21:07:58 +0500

    Working programm 4

---
+++ Task8/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+while (count <= a)
+{
+    Console.WriteLine(count);
+    count = count + 2;
+}
+
+if(a < 1)
+{
+   Console.WriteLine("Вы ввели недопустимое значение.");  
+}
+
+if(a == 1)
+{
+   Console.WriteLine("Четных чисел в этом диапазоне нет.");  
+}
+
